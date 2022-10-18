9e069d2cc60fecea59422ad3931213eaeed1079f
jlozanosu <112210496+jlozanosu@users.noreply.github.com>
2022-10-17 22:34:22 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -172,11 +172,10 @@ def pregunta_10():
     3   D                  1:2:3:5:5:7
     4   E  1:1:2:3:3:4:5:5:5:6:7:8:8:9
     """
-    #tenemos los valores unicos
+    
     xx = sorted(tbl0["_c1"].unique())
     resultado={}
-    #usamos un filtro para cada uno de los datos anteriores y agrupamos en diccionario
-    #La funcion map nos aplica la funcion str a cada elemento del filtro
+    
     for i in xx:
         resultado[str(i)]=":".join(map(str, sorted(tbl0[tbl0["_c1"]==i]["_c2"])))
 
@@ -208,15 +207,14 @@ def pregunta_11():
     38   38      d,e
     39   39    a,d,f
     """
-    #tenemos los valores unicos
+    
     xx = sorted(tbl1["_c0"].unique())
     resultado={}
-    #usamos un filtro para cada uno de los datos anteriores y agrupamos en diccionario
-    #La funcion map nos aplica la funcion str a cada elemento del filtro
+    
     for i in xx:
         resultado[str(i)]=",".join(map(str, sorted(tbl1[tbl1["_c0"]==i]["_c4"])))
 
-    #Con esto se transforma de diccionario a dataframe
+    
     df=pd.DataFrame(list(resultado.items()),
                    columns=['_c0', '_c4'])
     return df
@@ -238,16 +236,15 @@ def pregunta_12():
     38   38                    eee:0,fff:9,iii:2
     39   39                    ggg:3,hhh:8,jjj:5
     """
-    #tenemos los valores unicos
+    
     xx = sorted(tbl2["_c0"].unique())
-    #Convertimos los int en str
+    
     tbl2["_c5b"]=tbl2["_c5b"].astype(str)
-    #Concadenamos las columnas
+    
     tbl22=tbl2.assign(_c5=tbl2._c5a +":" +tbl2._c5b)
     print(xx)
     resultado={}
-    #usamos un filtro para cada uno de los datos anteriores y agrupamos en diccionario
-    #La funcion map nos aplica la funcion str a cada elemento del filtro
+    
     for i in xx:
         resultado[str(i)]=",".join(map(str, sorted(tbl22[tbl22["_c0"]==i]["_c5"])))
 
@@ -257,8 +254,6 @@ def pregunta_12():
                    
     return df
     
-
-
 
 def pregunta_13():
     """
@@ -274,12 +269,12 @@ def pregunta_13():
     E    275
     Name: _c5b, dtype: int64
     """
-    #se unen las tablas
+    
     xx=pd.merge(
         tbl0,
         tbl2,
         on="_c0"
     )
-    #Se agrupa
+    
     xxx=xx.groupby("_c1")["_c5b"].sum()
     return xxx
